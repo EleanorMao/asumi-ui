e60f5098b4040df0a37351d6c8868b2bdc3c72c0
Elly <danningmao@outlook.com>
2017-11-30 23:15:16 +0800

    add EditorConfig & config jest

--- __tests__/setup.js
+++ __tests__/setup.js
@@ -1,9 +1,7 @@
-
 require('raf/polyfill');
 const Enzyme = require('enzyme');
 const Adapter = require('enzyme-adapter-react-16');
 
-
-Enzyme.configure({ adapter: new Adapter() });
+Enzyme.configure({adapter: new Adapter()});
 
 
