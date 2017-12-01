9f43e43c044b7e9c8434de39ef64bc82aee7ecbb
Elly <danningmao@outlook.com>
2017-12-01 23:11:29 +0800

    testuplpad

---
+++ __tests__/upload.test.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import {shallow} from 'enzyme';
+import {Upload} from '../src';
+
+describe('Upload', () => {
+    it('create component', () => {
+        const component = shallow(<Upload/>);
+        expect(component).toBeDefined();
+        expect(component.find('.el-uploader-wrapper input[type="file"]').length).toEqual(1);
+    });
+    it('define className', () => {
+        const component = shallow(<Upload className="jest"/>);
+        expect(component).toBeDefined();
+        expect(component.find('.el-uploader-wrapper.jest').length).toEqual(1);
+    });
+    it('define name', () => {
+        const component = shallow(<Upload name="name"/>);
+        expect(component).toBeDefined();
+        expect(component.find(".el-uploader-wrapper input[name='name']").length).toEqual(1);
+    });
+    it('define accept & typeValidatorError', () => {
+        const handleValidatorError = jest.fn();
+        const validatorError = jest.fn();
+        const component = shallow(<Upload accept="image/*" validatorError={validatorError}
+                                          typeValidatorError={handleValidatorError}/>);
+        expect(component).toBeDefined();
+        expect(component.find('.el-uploader-wrapper input[accept="image/*"]').length).toEqual(1);
+        component.find('input').simulate('change', {
+            target: {
+                files: [{filename: 'foo.png', type: "image/png"},],
+            },
+            preventDefault: jest.fn()
+        });
+        component.find('input').simulate('change', {
+            target: {
+                files: [{filename: 'foo.html', type: "text/html"},],
+            },
+            preventDefault: jest.fn()
+        });
+        jest.runAllTimers();
+        expect(handleValidatorError).toBeCalled();
+        expect(validatorError).toBeCalled();
+    });
+    it('define maxSize & sizeValidatorError', () => {
+        const handleValidatorError = jest.fn();
+        const validatorError = jest.fn();
+        const component = shallow(<Upload maxSize={1024} validatorError={validatorError}
+                                          sizeValidatorError={handleValidatorError}/>);
+        expect(component).toBeDefined();
+        expect(component.find('.el-uploader-wrapper input').length).toEqual(1);
+        component.find('input').simulate('change', {
+            target: {
+                files: [{filename: 'foo.png', size: 100},],
+            },
+            preventDefault: jest.fn()
+        });
+        component.find('input').simulate('change', {
+            target: {
+                files: [{filename: 'foo.html', size: 2000},],
+            },
+            preventDefault: jest.fn()
+        });
+        jest.runAllTimers();
+        expect(handleValidatorError).toBeCalled();
+        expect(validatorError).toBeCalled();
+    });
+    it('define disabled', () => {
+        const component = shallow(<Upload disabled/>);
+        expect(component).toBeDefined();
+        expect(component.find('.el-uploader-wrapper input[disabled]').length).toEqual(1);
+    });
+    it('define multiple', () => {
+        const component = shallow(<Upload multiple/>);
+        expect(component).toBeDefined();
+        expect(component.find('.el-uploader-wrapper input[multiple]').length).toEqual(1);
+    });
+    it('define children', () => {
+        const component = shallow(<Upload><b/></Upload>);
+        expect(component).toBeDefined();
+        expect(component.find('.el-uploader-wrapper b').length).toEqual(1);
+    });
+    it('define onUpload', () => {
+        const handleUpload = jest.fn();
+        const component = shallow(<Upload onUpload={handleUpload}/>);
+        expect(component).toBeDefined();
+        expect(component.find('.el-uploader-wrapper input').length).toEqual(1);
+        component.find('input').simulate('change', {
+            target: {
+                files: [{filename: 'foo.png', size: 100},],
+            },
+            preventDefault: jest.fn()
+        });
+        jest.runAllTimers();
+        expect(handleUpload).toBeCalled();
+    });
+    it('define validator & validatorError', () => {
+        const validator = () => {
+            return false
+        };
+        const validatorError = jest.fn();
+        const component = shallow(<Upload validator={validator} validatorError={validatorError}/>);
+        expect(component).toBeDefined();
+        expect(component.find('.el-uploader-wrapper input').length).toEqual(1);
+        component.find('input').simulate('change', {
+            target: {
+                files: [{filename: 'foo.png'},],
+            },
+            preventDefault: jest.fn()
+        });
+        jest.runAllTimers();
+        expect(validatorError).toBeCalled();
+    });
+});
